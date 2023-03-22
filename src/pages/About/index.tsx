9d3ddd0d45c76486cceb0a8b418b72698b621e45
Eva1ent <let@ik.me>
2023-03-23 05:54:36 +0800

    Update version number in tauri.conf.json and add logo and app info in About page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,9 +1,32 @@
-import { styled } from '@stitches/react'
+import { Image, Text } from '@mantine/core'
+import { getName, getVersion } from '@tauri-apps/api/app'
+import { useAsync } from 'react-use'
 
-const Container = styled('div', {})
+import { styled } from '@/theme'
+
+const Container = styled('div', {
+    display: 'flex',
+    flexFlow: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+    padding: '8px 16px'
+})
 
 const About = () => {
-    return <Container />
+    const { value: name } = useAsync(getName)
+    const { value: version } = useAsync(getVersion)
+
+    return (
+        <Container>
+            <Image src="/icon.png" alt="logo" height="44px" width="auto" />
+            <Text size={14}>{name}</Text>
+            <Text size={10}>Version: {version}</Text>
+            <Text size={10} align="center">
+                Copyright © 2023 Eva1ent. All rights reserved.
+            </Text>
+        </Container>
+    )
 }
 
 About.displayName = 'About'
