35202ec6f7efe58cc23677955a04ab8fcdc8ef7b
Eva1ent <let@ik.me>
2023-07-07 01:00:50 +0800

    chore: update package.json and config.ts to use constants
    - Update the version of "vite" in package.json from "4.4.0" to "4.4.1"
    - Update the values of "sen", "accEnabled", and "locale" in src/config.ts to use constants from ./constants
    
    Signed-off-by: Eva1ent <let@ik.me>

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,15 @@
 import { Config } from "@/types"
 
+import { DEFAULT_ACC_ENABLED, DEFAULT_LOCALE, DEFAULT_SEN } from "./constants"
 import { type Locales } from "./i18n/i18n-types"
 import { ConfigManager } from "./lib/config"
 
 export const defaultConfig: Config & {
     locale: Locales
 } = {
-    sen: 90,
-    accEnabled: false,
-    locale: "en",
+    sen: DEFAULT_SEN,
+    accEnabled: DEFAULT_ACC_ENABLED,
+    locale: DEFAULT_LOCALE,
     theme: "light",
 }
 
