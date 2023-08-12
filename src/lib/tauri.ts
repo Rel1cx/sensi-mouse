8b14d7412d34c4529d2bab8366341f11d1a2d135
Eva1ent <let@ik.me>
2023-08-13 05:56:29 +0800

    chore: update deps

--- src/lib/tauri.ts
+++ src/lib/tauri.ts
@@ -2,7 +2,6 @@ import { Result } from "@swan-io/boxed"
 import { invoke as tauriInvoke } from "@tauri-apps/api"
 import { type InvokeArgs } from "@tauri-apps/api/tauri"
 
-// eslint-disable-next-line etc/no-misused-generics
 export const invoke = <T = void>(cmd: string, payload?: InvokeArgs) => {
     return Result.fromPromise<T, Error>(tauriInvoke<T>(cmd, payload))
 }
